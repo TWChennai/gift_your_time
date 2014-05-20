59b561542126cebac2f813126efc0bb198604f0d
sharanya <sharans@thoughtworks.com>
2014-05-20 12:53:36 +0530

    Fixed events load on second time home page visit by removing turbolinks

--- db/schema.rb
+++ db/schema.rb
@@ -24,13 +24,6 @@ ActiveRecord::Schema.define(version: 20140514113421) do
     t.datetime "updated_at"
   end
 
-  create_table "event_participation", force: true do |t|
-    t.integer  "event_id"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "event_participations", force: true do |t|
     t.integer  "event_id"
     t.integer  "user_id"
