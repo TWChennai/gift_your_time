ec0e05fa0a1d656180fe023704485ca855c168da
Chandrasekar <cswamin@thoughtworks.com>
2014-05-28 01:57:46 +0530

    Fixed the top menu. Changed the way it appears in mobile res -Chandru

--- app/assets/javascripts/notification_controller.js
+++ app/assets/javascripts/notification_controller.js
@@ -2,18 +2,17 @@ window.NotificationController = function(noticeMsgsSelector, alertMsgsSelector) 
     var noticeMsgs = $(noticeMsgsSelector).contents();
     var alertMsgs = $(alertMsgsSelector).contents();
     this.showNotifications = function() {
+        var notyOptions = {
+            timeout: 2000
+        };
         if (noticeMsgs.length > 0) {
-            noty({
-                text: noticeMsgs[0],
-                timeout: 2000,
-                type: "success"
-            });
+            notyOptions["text"] = noticeMsgs[0];
+            notyOptions["type"] = "sucess";
+            noty(notyOptions);
         }
         if (alertMsgs.length > 0) {
-            noty({
-                text: alertMsgs[0],
-                timeout: true
-            });
+            notyOptions["text"] = alertMsgs[0];
+            noty(notyOptions);
         }
     };
 };
